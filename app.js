500394b85fc6c215420d08c35b59ea969141e635
Jorge Rios <jorgeluisrios47@gmail.com>
2023-05-25 11:09:16 -0400

    connected mongo with mongoose

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express')
-const morgan = require('morgan') 
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const { result } = require('lodash')
 
 // experss app
 const app = express()
 
 // connect to mongoDB
 const dbURI = 'mongodb+srv://jrios:1234@nodetuts.qydyih9.mongodb.net/'
+mongoose.connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err))
 
 // register view engine
 app.set('view engine', 'ejs')
-
-// listen for requests
-app.listen(3000)
 
 // middleware & static files
 app.use(express.static('public'))
